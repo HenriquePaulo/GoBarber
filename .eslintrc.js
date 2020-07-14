3a1d244a1e09dba83d9c2eaffb43e66758de6ef6
Paulo <henrique.paulo307@hotmail.com>
2020-07-14 18:54:04 -0300

    primeiro commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  rules: {
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    camelcase: "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+    "linebreak-style": 0,
+    "prettier/prettier": "error",
+  },
+};
